0f3608b374b9d790ab77c4d8701419fb406a2ee1
buyaramadhan <ytchochote@gmail.com>
2023-06-22 11:11:51 +0700

    commit11

--- linklistqueue/linklistqueue.cpp
+++ linklistqueue/linklistqueue.cpp
@@ -70,7 +70,18 @@ public :
 			return;
 		}
 
-		cout << ""
-	
+		cout << "\nElement in the queue are: \n";
+
+		// jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+		else {
+			//jika FRONT_position > REA_position, iterarsi dari FRONT hingga akhir array
+		}
 	}
 };
