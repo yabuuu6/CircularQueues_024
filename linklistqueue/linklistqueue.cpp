4a658a2aada69446d4be53b6b24eef2642f7e20f
buyaramadhan <ytchochote@gmail.com>
2023-06-22 10:35:26 +0700

    Add project files.

---
+++ linklistqueue/linklistqueue.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Queues {
+	int FRONT, RAER, max = 5;
+	inte queue_array(5);
+
+public :
+	Queues() {
+		FRONT = -1;
+		REAR = -1;
+	}
+
+	void insert() {
+		int num;
+		cout << "Enter a number: ";
+	}
+};
