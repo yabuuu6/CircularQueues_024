a43160fae7b039e101cd8cb2b6096d6bd5dd4634
buyaramadhan <ytchochote@gmail.com>
2023-06-22 11:25:08 +0700

    commit16

--- linklistqueue/linklistqueue.cpp
+++ linklistqueue/linklistqueue.cpp
@@ -108,6 +108,15 @@ int main() {
 			cout << "Menu" << endl;
 			cout << "1. Implement insert oprator" << endl;
 			cout << "2. Implement delete optration" << endl;
+			cout << "3. Display values" << endl;
+			cout << "4.Exit" << endl;
+			cout << "Enter your choice (1-4): ";
+			cin >> ch;
+			cout << endl;
+
+			switch (ch) {
+				case 
+			}
 		}
 	}
 }
